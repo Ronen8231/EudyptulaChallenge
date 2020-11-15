35d5fb5bf3b059d1eebc9c2632948c9bf6afe1c3
Ronen Hanukayev <ronenh8231@gmail.com>
2020-11-15 21:09:15 +0200

    task 1

---
+++ task1/task1.c
@@ -0,0 +1,26 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#define DRIVER_AUTHOR "Ronen Hanukayev <ronenh8231@gmail.com>"
+#define DRIVER_DESCRIPTION "My first linux kernel module :)"
+#define DRIVER_LICENSE "GPL"
+
+static int __init task1_init(void)
+{
+    printk(KERN_DEBUG "Hello World!\n");
+    return 0;
+}
+
+static void __exit task1_exit(void)
+{
+    printk(KERN_DEBUG "Goodbye world.\n");
+}
+
+module_init(task1_init);
+module_exit(task1_exit);
+
+MODULE_LICENSE(DRIVER_LICENSE);
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
+MODULE_SUPPORTED_DEVICE("testdevice");
